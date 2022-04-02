fb0ecb8bbf51b5d306741d7902d5a443ab83e211
Ocammy <lfgalvanuribe@outlook.com>
2022-04-02 11:33:10 -0600

    *Se modificó el archivo HomeControlador, agregando los siguientes metodos: -TramaConfirmacionO //Para retornar a una vista. -TramaVolumenTanques() //Para retornar a una vista. -GetCatalogos2() //Para generar un catalogo-selecet. -GetComportamientos() //Para generar un catalogo-selecet. -GetEstadoTran() //Para generar un catalogo-selecet. -GetModulo() //Para generar un catalogo-selecet. -GetProducto() //Para generar un catalogo-selecet. -TramaConfirmacionO(TramaConfOrdenCargaDescViewModel model) //Para generar un registro en la base de datos. -TramaVolumenTanques(TramaVolumenesTanquesViewModel model) //Para generar un registro en la base de datos. -InterpretarTramaS(string id) //Para mandar a llamar un registro de la base de datos y usarlo para interpretarse con otro metodo. -leertramaConfirmada(string Trama) //Para interpretar un registro traido de la base de datos. -leerTramaVolumenes(string Trama2) //Para interpretar un registro traido de la base de datos.
    *Se modifició el archivo Site.css para generar un estilo para las validaciones basicas de las tramas.
    *Se generó el archivo auditoriaViewModel.
    *Se modificó el archivo salidasViewModel.
    *Se modificó el archivo TramaViewModel.
    *Se modificaron todas las Views, agregando lo necesario para la generacion de formularios y tablas que retornan datos de la BD.

--- Models/ViewModel/TramaViewModel.cs
+++ Models/ViewModel/TramaViewModel.cs
@@ -3,31 +3,32 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace TAS360.Models.ViewModel
 {
     public class TramaConfOrdenCargaDescViewModel
-    {
+    {       
         [Required]
-        [Display(Name = "Tipo de Transaccion")]
-        
+        [Display(Name = "Tipo de Transaccion")]        
         public string Tipo_Transaccion { get; set; }
 
         [Required]
         [Display(Name = "Numero de Operacion")]
-        [StringLength(5, ErrorMessage = "El {0} debe tener al menos {1} caracter", MinimumLength = 1)]
+        [StringLength(5, ErrorMessage = "El codigo de operación no puede exeder los 5 digitos.")]
         public string Numero_Operacion { get; set; }
 
         [Required]
-        [Display(Name = "Numero de compartimiento")]
+        [Display(Name = "Compartimiento")]
         public string Numero_Compartimiento { get; set; }
 
-        [Required]
+        [Required] 
         [Display(Name = "Estado de transaccion")]
         public string Estado_Transaccion { get; set; }
 
         [Required]
         [Display(Name = "Razones")]
+        [StringLength(2, ErrorMessage = "El codigo de razones no debe exeder los 2 digitos.")]
         public string Razones { get; set; }
 
         [Required]
@@ -36,23 +37,26 @@ namespace TAS360.Models.ViewModel
 
         [Required]
         [Display(Name = "Volumen natural")]
-        public double volumen_natural { get; set; }
+        [StringLength(11, ErrorMessage = "El codigo de voluumen natural no debe exeder los 11 digitos.")]
+        public string volumen_natural { get; set; }
 
         [Required]
         [Display(Name = "Volumen neto")]
-        public double volumen_neto { get; set; }
+        [StringLength(11, ErrorMessage = "El codigo de volumen neto no debe exeder los 2 digitos.")]
+        public string volumen_neto { get; set; }
 
         [Required]
         [Display(Name = "Temperatura")]
-        public double temperatura { get; set; }
+        [StringLength(5, ErrorMessage = "La temperatura debe registrarse como 99.99 o 999.9.")]  
+        public string temperatura { get; set; }
 
         [Required]
         [Display(Name = "Codigo Anterior de Producto")]
-        public string codigo_Anterior_producto { get; set; }
+        public string Codigo_Anterior_producto { get; set; }
 
         [Required]
         [Display(Name = "Fecha de inicio")]
-        public DateTime Fecha_inicio { get; set; }
+        public string Fecha_inicio { get; set; }
 
         [Required]
         [Display(Name = "Hora de inicio")]
@@ -60,29 +64,78 @@ namespace TAS360.Models.ViewModel
 
         [Required]
         [Display(Name = "Fecha fin")]
-        public DateTime Fecha_fin { get; set; }
+        public string Fecha_fin { get; set; }
 
         [Required]
         [Display(Name = "Hora de fin")]
-        [StringLength(4, ErrorMessage = "El {0} debe tener al menos {1} caracteres", MinimumLength = 4)]
         public string hora_fin { get; set; }
 
         [Required]
         [Display(Name = "Posicion de carga")]
+        [StringLength(2, ErrorMessage = "El codigo de posicion de carga no debe exeder los 2 digitos.")]
         public string posicion_carga { get; set; }
 
         [Required]
         [Display(Name = "Flujo promedio")]
+        [StringLength(4, ErrorMessage = "El codigo de flujo promedio no debe exeder los 4 digitos.")]
         public string flujo_promedio { get; set; }
 
         [Required]
         [Display(Name = "Factor de medicion")]
+        [StringLength(6, ErrorMessage = "El codigo de factor de medicion debe ingresarse como 9.9999 .")]
         public string factor_medicion { get; set; }
 
         [Required]
         [Display(Name = "Codigo nuevo de Producto")]
-        public string codigo_nuevo_producto { get; set; }
-
-       
+        public string Codigo_nuevo_producto { get; set; }
     }
+
+    public class TramaVolumenesTanquesViewModel
+    {
+        [Required]
+        [Display(Name = "Tipo de Transaccion")]
+        public string Tipo_Transaccion { get; set; }
+
+        [Required]
+        [Display(Name = "Numero de tanque")]
+        [StringLength(3, ErrorMessage = "El codigo de numero de tanque no puede exeder los 3 digitos.")]
+        public string numero_tanque { get; set; }
+
+        [Required]
+        [Display(Name = "Codigo de Producto Anterior")]
+        public string codigo_Anterior_producto { get; set; }
+
+        [Required]
+        [Display(Name = "Volumen Total Neto")]
+        [StringLength(11, ErrorMessage = "El codigo de volumen total neto no puede exeder los 11 digitos.")]
+        public string volumen_total_neto { get; set; }
+
+        [Required]
+        [Display(Name = "Volumen neto")]
+        [StringLength(11, ErrorMessage = "El codigo de volumen neto no puede exeder los 11 digitos.")]
+        public string volumen_neto { get; set; }
+
+        [Required]
+        [Display(Name = "Volumen natural")]
+        [StringLength(11, ErrorMessage = "El codigo de volumen natural no puede exeder los 11 digitos.")]
+        public string volumen_natural { get; set; }
+
+        [Required]
+        [Display(Name = "Temperatura promedio")]
+        [StringLength(5, ErrorMessage = "La temperatura debe registrarse como 99.99 o 999.9 .")]
+        public string temperatura_promedio { get; set; }
+
+        [Required]
+        [Display(Name = "Fecha")]
+        public string fecha { get; set; }
+
+        [Required]
+        [Display(Name = "Hora")]
+        public string Hora { get; set; }
+
+        [Required]
+        [Display(Name = "Codigo nuevo de Producto.")]
+        public string codigo_nuevo_producto { get; set; }
+    }
+
 }
--- Models/ViewModel/salidasViewModel.cs
+++ Models/ViewModel/salidasViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace TAS360.Models.ViewModel
 {
@@ -10,26 +11,45 @@ namespace TAS360.Models.ViewModel
         public salidasViewModel()
         {
             salidas = new List<Salidas>();
-            Trama = new ordenCargaDescarga();
+            Trama = new ordenCargaDescargaSalidas();
+            Trama2 = new volumenTanques();
         }
-       public List<Salidas> salidas { get; set; }
-        public ordenCargaDescarga Trama { get; set; }
-        
+        public List<Salidas> salidas { get; set; }
+        public ordenCargaDescargaSalidas Trama { get; set; }
+        public volumenTanques Trama2 { get; set; }
     }
-
-   public class ordenCargaDescarga
+   public class ordenCargaDescargaSalidas
     {
         public string TipoTransaccion { get; set; }
         public string NumeroOperacion { get; set; }
         public string NumeroCopartimiento { get; set; }
         public string EstadoTransaccion {  get; set; }
-        public string RazonCancelacion { get; set; }
-        public string ModuloOperqcion { get; set; }
-        public string identificacionVehiculo { get; set; }
-        public string codigoProductoAnterior { get; set; }
+        public string Razon { get; set; }
+        public string ModuloOperacion { get; set; }
         public string VolumenNatural { get; set; }
-        public string ClaveCliente { get; set; }
+        public string VolumenNeto { get; set; }
+        public string Temperatura { get; set; }
+        public string CodigoProductoAnterior { get; set; }
+        public string FechaInicio { get; set; }
+        public string HoraInicio { get; set; }
+        public string FechaTermino { get; set; }
+        public string HoraTermino { get; set; }
+        public string PosicionCarga { get; set; }
+        public string FlujoPromedio { get; set; }
+        public string FactorMedicion { get; set; }
         public string CodigoProductoNuevo { get; set; }
-        public string densidad { get; set; }
+    }
+    public class volumenTanques
+    {
+        public string TipoTransaccion { get; set; }
+        public string NumeroTanque { get; set; }
+        public string CodigoProductoAnterior { get; set; }
+        public string VolumenTotalNeto { get; set; }
+        public string VolumenNeto { get; set; }
+        public string VolumenNatural { get; set; }
+        public string TemperaturaPromedio { get; set; }
+        public string Fecha { get; set; }
+        public string Hora { get; set; }
+        public string CodigoProductoNuevo { get; set; }
     }
 }
