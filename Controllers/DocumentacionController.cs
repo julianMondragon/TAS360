7a03cbb6a353146e08af505cbf55da15e030a8ab
Julian_Mondragon <mondragon2712@gmail.com>
2024-04-11 19:57:01 -0600

    Content              Manuales               Se actualizan los archivos de los manuales
                  Img
                  Se anexan imagenes para la pantalla de Procedimientos y Manuales
    
    Controllers
    Se agrega el controlador de Documentacion.
    
    Vistas
           Documentacion
                Se crea la vista de Manuales.
                Se crea la vista de Procedimientos.
            Home
                Se modifica el home añadiendo un boton para redirigir hacia los manuales y procedimientos
             Shared
                 Se modifica la version.

---
+++ Controllers/DocumentacionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TAS360.Controllers
+{
+    public class DocumentacionController : Controller
+    {
+        // GET: Documentacion
+        public ActionResult Procedimientos()
+        {
+            return View();
+        }
+
+        public ActionResult Manuales()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/LSAController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TAS360.Controllers
+{
+    public class LSAController : Controller
+    {
+        // GET: LSA
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
