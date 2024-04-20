5ad4d70487ad6e5073fb2d8d38e4026845071e48
Julian_Mondragon <mondragon2712@gmail.com>
2024-04-19 21:23:36 -0600

    Controllers Tickets Se ordena la lista de Tickets de forma desendente y se excluyen los tickets Cerrados. LSA Se crea un Metodo de tipo httpGet que se encarga de obtener un resumen de los tickets por status del ultimo mes. Se crea un Metodo de tipo httpGet que se encarga de obtener un resumen de los tickets por categoria. se crea un metodo que devuelve a la vista el resumen de los tickets abiertos y cerrados.
    ViewModel
    Se crea el modelo para graficar los tickets por status del ultimo mes.
    Se crea el modelo para graficar los tickets por Categoria.
    
    View
    LSA/Index
    Se agregan las graficas para monitorear los tickets por status
    Se agrega funcion en ajax para mandar a llamar el metodo que ejecuta el sP para devolver los datos a la vista mediante un json.
    Tickets/index
    Se renombran los encabezados de la tabla tickets
    
    StoreProc
    LSA_Reportes
    metodo que ejecuta el SP para devolver los Ticket por status del ultimo mes a la vista mediante un json.
    metodo que ejecuta el SP para devolver los Ticket por Categoria a la vista mediante un json.

--- Controllers/LSAController.cs
+++ Controllers/LSAController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TAS360.Models.ViewModel;
+using TAS360.StorProc;
 
 namespace TAS360.Controllers
 {
@@ -11,7 +13,47 @@ namespace TAS360.Controllers
         // GET: LSA
         public ActionResult Index()
         {
+            GetSummaryTKs();
             return View();
+        }
+        [HttpGet]
+        public JsonResult GetTicktsByStatus()
+        {
+            LSA_Reportes Tickets = new LSA_Reportes();
+            List<G_TicketsByStatusViewModel> listTksbyStatus = Tickets.GetTicktsByStatus();
+
+            return Json(listTksbyStatus, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public JsonResult GetTicketsByCategoria()
+        {
+            LSA_Reportes Tickets = new LSA_Reportes();
+            List<G_TicketsByCategoriaViewModel> listTksbyCategoria = Tickets.GetTicketsByCategoria();
+
+            return Json(listTksbyCategoria, JsonRequestBehavior.AllowGet);
+        }
+        private void GetSummaryTKs()
+        {
+            int tksopen = 0;
+            int tksclose = 0;
+            List<TicketViewModel> tickets = new List<TicketViewModel>();
+            using (Models.HelpDesk_Entities1 db = new Models.HelpDesk_Entities1())
+            {
+                var Tickets = (from s in db.Ticket select s);
+                if (Tickets != null && Tickets.Any())
+                {
+                    foreach (var t in Tickets)
+                    {
+                        if (t.status == 12)
+                            tksclose++;
+                        else
+                            tksopen++;
+
+                    }
+                }
+            }
+            ViewBag.tksopen = tksopen;
+            ViewBag.tksclose = tksclose;
         }
     }
 }
---
+++ Models/ViewModel/G_TicketsByCategoriaViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TAS360.Models.ViewModel
+{
+    public class G_TicketsByCategoriaViewModel
+    {
+        public string Categoria { get; set; }
+        public int Cantidad { get; set; }
+        public DateTime Mas_Reciente { get; set; }
+        public DateTime Mas_Antiguo { get; set; }
+    }
+}
