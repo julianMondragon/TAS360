fab0362513f4bd2aed38308631a763eebf025405
irving julian mondragon <mondragon2712@gmail.com>
2024-08-13 00:26:06 -0600

    Controller Se agrega el controlador de Roles y privilegios
    ViewModel
    Se agrega el ViewModel para mostrar los Roles y privilegios
    
    View
    Se agrega la vista Roles y Privilegios
    Se agrega las tablas de los Roles, Operaciones, Modulos
    
    Shared
    Se agrega al menu el link para los roles y privilegios

---
+++ Controllers/Roles_and_PrivilegeController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TAS360.Models;
+using TAS360.Models.ViewModel;
+
+namespace TAS360.Controllers
+{
+    public class Roles_and_PrivilegeController : Controller
+    {
+        /// <summary>
+        ///  Metodo principal que muestra los modulos, roles y privilegios 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            RolesPrivilegesViewModel rolesPrivileges = new RolesPrivilegesViewModel();
+            using (HelpDesk_Entities1 db = new HelpDesk_Entities1())
+            {
+                var modules = from m in db.Modulo select m;
+                if (modules.Any())
+                {
+                    foreach (var item in modules)
+                    {
+                        rolesPrivileges.Modules.Add(item);
+                    }
+                }
+                var roles = from r in db.Roll select r;
+                if (roles.Any())
+                {
+                    foreach (var item in roles)
+                    {
+                        rolesPrivileges.Rolls.Add(item);
+                    }
+                }
+                var operacion = from r in db.Operacion select r;
+                if (roles.Any())
+                {
+                    foreach (var item in operacion)
+                    {
+                        rolesPrivileges.Operacions.Add(item);
+                    }
+                }
+                var rol_operacionA = from R in db.Roll_Operacion where R.id_Roll == 1 select R;
+                if (rol_operacionA.Any())
+                {
+                    foreach( var item in rol_operacionA)
+                    {
+                        rolesPrivileges.Rol_OperacionAdmin.Add(item);
+                    }
+                }
+                var rol_operacionR = from R in db.Roll_Operacion where R.id_Roll == 2 select R;
+                if (rol_operacionR.Any())
+                {
+                    foreach (var item in rol_operacionR)
+                    {
+                        rolesPrivileges.Rol_OperacionResp.Add(item);
+                    }
+                }
+                var rol_operacionC = from R in db.Roll_Operacion where R.id_Roll == 3 select R;
+                if (rol_operacionC.Any())
+                {
+                    foreach (var item in rol_operacionC)
+                    {
+                        rolesPrivileges.Rol_OperacionContac.Add(item);
+                    }
+                }
+                var rol_operacionV = from R in db.Roll_Operacion where R.id_Roll == 4 select R;
+                if (rol_operacionV.Any())
+                {
+                    foreach (var item in rol_operacionV)
+                    {
+                        rolesPrivileges.Rol_OperacionVist.Add(item);
+                    }
+                }
+
+            }
+            GetUsuarios();
+            return View(rolesPrivileges);
+        }
+        /// <summary>
+        /// Devuelve a la vista una lista de los usuarios especialistas tecnicos 
+        /// </summary>
+        private void GetUsuarios()
+        {
+
+            List<SelectListItem> Usuarios = new List<SelectListItem>();
+            using (HelpDesk_Entities1 db = new HelpDesk_Entities1())
+            {
+                var aux = (from s in db.User select s);
+                if (aux != null && aux.Any())
+                {
+                    foreach (var a in aux)
+                    {
+                        Usuarios.Add(new SelectListItem
+                        {
+                            Text = a.nombre,
+                            Value = a.id.ToString()
+
+                        });
+                    }
+                }
+            }
+            ViewBag.Usuarios = Usuarios;
+
+        }
+    }
+}
---
+++ Models/ViewModel/RolesPrivilegesViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TAS360.Models;
+
+namespace TAS360.Models.ViewModel
+{
+    public class RolesPrivilegesViewModel
+    {
+        public RolesPrivilegesViewModel()
+        {
+            this.Rolls = new List<Roll>();
+            this.Modules = new List<Modulo>();
+            this.Rol_OperacionAdmin = new List<Roll_Operacion>();
+            this.Rol_OperacionResp = new List<Roll_Operacion>();
+            this.Rol_OperacionContac = new List<Roll_Operacion>();
+            this.Rol_OperacionVist = new List<Roll_Operacion>();
+            this.Operacions = new List<Operacion>();
+        }
+        public List<Roll> Rolls { get; set; }
+        public List<Modulo> Modules { get; set; }
+        public List<Roll_Operacion> Rol_OperacionAdmin { get; set; }
+        public List<Roll_Operacion> Rol_OperacionResp { get; set; }
+        public List<Roll_Operacion> Rol_OperacionContac { get; set; }
+        public List<Roll_Operacion> Rol_OperacionVist { get; set; }
+        public List<Operacion> Operacions { get; set; }
+    }
+}
